5e4d4f7add6127dcc276b02767272baacab078ff
ajtock <andytock@gmail.com>
2021-02-23 17:13:11 +0000

    extract_EDTA_noncentromeric_TE_superfamilies

--- T2T_Col/annotation/TEs/extract_EDTA_noncentromeric_TE_superfamilies.R
+++ T2T_Col/annotation/TEs/extract_EDTA_noncentromeric_TE_superfamilies.R
@@ -1,11 +1,12 @@
 #!/applications/R/R-4.0.0/bin/Rscript
 
 # Create tables of elements within each transposable element superfamily
+# not located within T2T_Col centromeres
 # and generate random loci of the same number and width distribution
 # Write as GFF3 and BED files 
 
 # Usage:
-# ./extract_EDTA_TE_superfamilies.R 'Chr1,Chr2,Chr3,Chr4,Chr5' Gypsy_LTR
+# ./extract_EDTA_noncentromeric_TE_superfamilies.R 'Chr1,Chr2,Chr3,Chr4,Chr5' Gypsy_LTR
 
 #chrName <- unlist(strsplit("Chr1,Chr2,Chr3,Chr4,Chr5",
 #                           split = ","))
@@ -38,91 +39,10 @@ CENGR <- GRanges(seqnames = chrs,
                                   end = CENend),
                  strand = "*")
 
-# Load table of TE coordinates in T2T_Col (derived from EDTA)
-TEs <- data.frame(readGFF("t2t-col.20201227.fasta.mod.EDTA.TEanno.gff3"))
-superfamNamesFull <- sort(unique(TEs$Classification))
-print(superfamNamesFull)
-# [1] "DNA"           "DNA/DTA"       "DNA/DTC"       "DNA/DTH"      
-# [5] "DNA/DTM"       "DNA/DTT"       "DNA/En-Spm"    "DNA/Harbinger"
-# [9] "DNA/HAT"       "DNA/Helitron"  "DNA/Mariner"   "DNA/MuDR"     
-#[13] "DNA/Pogo"      "DNA/Tc1"       "LINE?"         "LINE/L1"      
-#[17] "LINE/unknown"  "LTR/Copia"     "LTR/Gypsy"     "LTR/unknown"  
-#[21] "MITE/DTA"      "MITE/DTC"      "MITE/DTH"      "MITE/DTM"     
-#[25] "MITE/DTT"      "RathE1_cons"   "RathE2_cons"   "RathE3_cons"  
-#[29] "RC/Helitron"   "SINE"          "Unassigned"    "Unknown"   
-
-# Get DNA elements
-DNA <- TEs[grep("DNA", TEs$Classification),]
-DNA <- rbind(DNA,
-             TEs[grep("RC/Helitron", TEs$Classification),],
-             TEs[grep("MITE", TEs$Classification),]
-            )
-DNA <- data.frame(DNA,
-                  DNA_RNA = "DNA")
-
-# Get unclassified elements
-Unclassified <- TEs[TEs$Classification %in% c("Unassigned", "Unknown"),]
-Unclassified <- data.frame(Unclassified,
-                           DNA_RNA = "Unclassified")
-
-# Get RNA elements
-RNA <- TEs[!(TEs$Classification %in% DNA$Classification)
-         & !(TEs$Classification %in% Unclassified$Classification),]
-RNA <- data.frame(RNA,
-                  DNA_RNA = "RNA")
-
-# Combine
-TEs <- rbind(DNA, RNA, Unclassified)
-
-# Make superfamily names filename-friendly
-TEs <- rbind(
-  data.frame(TEs[TEs$Classification == "DNA/En-Spm"
-               | TEs$Classification == "DNA/DTC"
-               | TEs$Classification == "MITE/DTC",],
-             superfamName = "EnSpm"),
-  data.frame(TEs[TEs$Classification == "DNA/Harbinger"
-               | TEs$Classification == "DNA/DTH"
-               | TEs$Classification == "MITE/DTH",],
-             superfamName = "Harbinger"),
-  data.frame(TEs[TEs$Classification == "DNA/HAT"
-               | TEs$Classification == "DNA/DTA"
-               | TEs$Classification == "MITE/DTA",],
-             superfamName = "hAT"),
-  data.frame(TEs[TEs$Classification == "DNA/Helitron"
-               | TEs$Classification == "RC/Helitron",],
-             superfamName = "Helitron"),
-  data.frame(TEs[TEs$Classification == "DNA/MuDR"
-               | TEs$Classification == "DNA/DTM"
-               | TEs$Classification == "MITE/DTM",],
-             superfamName = "MuDR"),
-  data.frame(TEs[TEs$Classification == "DNA/Pogo"
-               | TEs$Classification == "DNA/Tc1"
-               | TEs$Classification == "DNA/Mariner"
-               | TEs$Classification == "DNA/DTT"
-               | TEs$Classification == "MITE/DTT",],
-             superfamName = "Pogo_Tc1_Mariner"),
-  data.frame(TEs[TEs$Classification == "DNA",],
-             superfamName = "Unclassified_DNA"),
-  data.frame(TEs[TEs$Classification == "LTR/Copia",],
-             superfamName = "Copia_LTR"),
-  data.frame(TEs[TEs$Classification == "LTR/Gypsy",],
-             superfamName = "Gypsy_LTR"),
-  data.frame(TEs[TEs$Classification == "LTR/unknown",],
-             superfamName = "Unclassified_LTR"),
-  data.frame(TEs[TEs$Classification == "LINE/L1",],
-             superfamName = "LINE1"),
-  data.frame(TEs[TEs$Classification == "LINE?"
-               | TEs$Classification == "LINE/unknown",],
-             superfamName = "Unclassified_LINE"),
-  data.frame(TEs[TEs$Classification == "SINE"
-               | TEs$Classification == "RathE1_cons"
-               | TEs$Classification == "RathE2_cons"
-               | TEs$Classification == "RathE3_cons",],
-             superfamName = "SINE"),
-  data.frame(TEs[TEs$Classification == "Unassigned"
-               | TEs$Classification == "Unknown",],
-             superfamName = "Unclassified")
-)
+# Load TEs
+TEs <- read.table(file = paste0("T2T_Col_TEs_All_",
+                                paste0(chrName, collapse = "_"), ".tsv"),
+                  header = T)
 
 superfamNames <- sort(unique(TEs$superfamName))
 print(superfamNames)
@@ -133,7 +53,7 @@ print(superfamNames)
 #[13] "Unclassified_LINE" "Unclassified_LTR"
 
 print(dim(TEs))
-#[1] 42927    22
+#[1] 42770    22
 
 # Create a list of TE superfamily DataFrames
 superfamList <- lapply(seq_along(superfamNames), function(x) {
@@ -144,7 +64,7 @@ for(x in seq_along(superfamList)) {
   tmp <- tmp + dim(superfamList[[x]])[1]
 }
 print(tmp)
-#[1] 42927
+#[1] 42770
 
 stopifnot(identical(dim(TEs)[1], as.integer(tmp)))
 
@@ -152,36 +72,39 @@ TEs <- TEs[TEs$seqid %in% chrName,]
 print(dim(TEs))
 #[1] 42770    22
 
-TEs$Parent <- "."
-
-# Order by chromosome, start coordinates and end coordinates
-TEs <- TEs[ order(TEs$seqid,
-                  TEs$start,
-                  TEs$end), ]
-#TEsDNA <- TEs[TEs$DNA_RNA == "DNA",]
-#TEsRNA <- TEs[TEs$DNA_RNA == "RNA",]
-write.table(TEs,
-            file = paste0("T2T_Col_TEs_All_",
-                          paste0(chrName, collapse = "_"), ".tsv"),
-            quote = F, sep = "\t", row.names = F, col.names = T)
-TEs_bed <- data.frame(chr = as.character(TEs[,1]),
-                      start = as.integer(TEs[,4]-1),
-                      end = as.integer(TEs[,5]),
-                      name = paste0(1:dim(TEs)[1], "_", TEs[,10]),
-                      score = as.character(TEs[,6]),
-                      strand = as.character(TEs[,7]))
-write.table(TEs_bed,
-            file = paste0("T2T_Col_TEs_All_",
-                          paste0(chrName, collapse = "_"), ".bed"),
-            quote = F, sep = "\t", row.names = F, col.names = F)
-
 TEsGR <- GRanges(seqnames = TEs$seqid,
                  ranges = IRanges(start = TEs$start,
                                   end = TEs$end),
-                 strand = TEs$strand)
+                 strand = TEs$strand,
+                 name = paste0(1:dim(TEs)[1], "_", TEs[,10]),
+                 score = as.character(TEs[,6]))
+
+TEs_CEN_overlaps <- findOverlaps(query = CENGR,
+                                 subject = TEsGR,
+                                 type = "any",
+                                 select = "all",
+                                 ignore.strand = TRUE)
+if(length(TEs_CEN_overlaps) > 0) {
+  nonCEN_TEsGR <- TEsGR[-subjectHits(TEs_CEN_overlaps)]
+} else {
+  nonCEN_TEsGR <- TEsGR
+}
+
+# Order by chromosome, start coordinates and end coordinates
+nonCEN_TEs_bed <- data.frame(chr = as.character(seqnames(nonCEN_TEsGR)),
+                             start = as.integer(start(nonCEN_TEsGR)-1),
+                             end = as.integer(end(nonCEN_TEsGR)),
+                             name = as.character(nonCEN_TEsGR$name),
+                             score = as.character(nonCEN_TEsGR$score),
+                             strand = as.character(strand(nonCEN_TEsGR)))
+write.table(nonCEN_TEs_bed,
+            file = paste0("T2T_Col_nonCEN_TEs_All_",
+                          paste0(chrName, collapse = "_"), ".bed"),
+            quote = F, sep = "\t", row.names = F, col.names = F)
+
 
 # Define function to select randomly positioned loci of the same
-# width distribution as TEsGR
+# width distribution as nonCEN_TEsGR
 ranLocStartSelect <- function(coordinates, n) {
   sample(x = coordinates,
          size = n,
@@ -195,22 +118,22 @@ options(scipen = 100)
 # ranLocGR contains the same number of loci per chromosome as TEsGR
 ranLocGR <- GRanges()
 for(i in 1:length(chrs)) {
-  TEsChrGR <- TEsGR[seqnames(TEsGR) == chrs[i]]
+  nonCEN_TEsChrGR <- nonCEN_TEsGR[seqnames(nonCEN_TEsGR) == chrs[i]]
   regionChrGR <- regionGR[seqnames(regionGR) == chrs[i]]
   # Contract regionChrGR so that random loci and 2-kb flanking regions
   # do not extend beyond chromosome ends
-  end(regionChrGR) <- end(regionChrGR)-max(width(TEsChrGR))-2000
+  end(regionChrGR) <- end(regionChrGR)-max(width(nonCEN_TEsChrGR))-2000
   start(regionChrGR) <- start(regionChrGR)+2000
   # Define seed so that random selections are reproducible
   set.seed(93750174)
   ranLocChrStart <- ranLocStartSelect(coordinates = unlist(lapply(seq_along(regionChrGR), function(x) {           
                                                              start(regionChrGR[x]) : end(regionChrGR[x])          
                                                            })),
-                                      n = length(TEsChrGR))
+                                      n = length(nonCEN_TEsChrGR))
   ranLocChrGR <- GRanges(seqnames = chrs[i],
                          ranges = IRanges(start = ranLocChrStart,
-                                          width = width(TEsChrGR)),
-                         strand = strand(TEsChrGR))
+                                          width = width(nonCEN_TEsChrGR)),
+                         strand = strand(nonCEN_TEsChrGR))
   ranLocGR <- append(ranLocGR, ranLocChrGR)
 }
 
