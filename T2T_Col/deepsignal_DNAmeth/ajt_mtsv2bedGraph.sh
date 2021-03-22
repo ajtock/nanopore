c2223282a5fef4820ff1b66752ac03991d6135e6
ajtock <andytock@gmail.com>
2021-03-22 19:13:06 +0000

    per-read tsv to bedgraph python script adapt

--- T2T_Col/deepsignal_DNAmeth/ajt_mtsv2bedGraph.sh
+++ T2T_Col/deepsignal_DNAmeth/ajt_mtsv2bedGraph.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 # Usage on node7:
-# csmit -m 20G -c 1 "bash ./ajt_mtsv2bedGraph.sh head_WT_deepsignalDNAmeth_95_30kb_MappedOn_T2T_Col CpG" 
+# csmit -m 50G -c 1 "bash ./ajt_mtsv2bedGraph.sh head_WT_deepsignalDNAmeth_95_30kb_MappedOn_T2T_Col CpG" 
 
 prefix=$1
 context=$2
 
-#/home/nm359/nanopore-methylation-utilities/mtsv2bedGraph.py -i ${filename}.tsv -c ${threshold} -g ${reference} -v |\
-#  sort -k1,1 -k2,2n | bgzip > ${output}.bed.gz
-#tabix -p bed ${output}.bed.gz
-
-#source activate ChIPseq_mapping
+source activate ChIPseq_mapping
 
 ajt_mtsv2bedGraph.py --input ${prefix}_${context}_raw.tsv \
                      --mod ${context} \
                      --call-threshold 0.02 \
                      --genome /home/ajt200/analysis/nanopore/T2T_Col/T2T_Col.fa \
                      --offset 0 \
-                     --verbose > ${prefix}_${context}.bed
-#  sort -k1,1 -k2,2n | bgzip > ${prefix}.bed.gz
-#tabix -p bed ${prefix}.bed.gz
+                     --verbose | \
+  sort -k1,1 -k2,2n | bgzip > ${prefix}_${context}.bed.gz
+tabix -p bed ${prefix}_${context}.bed.gz
 
-#conda deactivate
+conda deactivate
