18f5141958b5158c8a179c8ff87df64d2e07c7e3
ajtock <andytock@gmail.com>
2021-10-29 15:23:54 +0100

    among read variation

--- t2t-col.20210610/deepsignal_DNAmeth/per_read_analysis/among_read_variation/among_read_variation_scoring.R
+++ t2t-col.20210610/deepsignal_DNAmeth/per_read_analysis/among_read_variation/among_read_variation_scoring.R
@@ -7,8 +7,8 @@
 
 # Usage on hydrogen node7:
 # chmod +x per_read_methylation_proportion_winByCs.R
-# csmit -m 300G -c 47 "/applications/R/R-4.0.0/bin/Rscript among_read_variation_scoring.R Col_0_deepsignalDNAmeth_30kb_90pc t2t-col.20210610 10000 500 CHG 0.50 0.66 Chr1"
-# csmit -m 200G -c 47 "/applications/R/R-4.0.0/bin/Rscript among_read_variation_scoring.R Col_0_deepsignalDNAmeth_30kb_90pc t2t-col.20210610 10000 500 CpG 0.50 0.66 Chr1"
+# csmit -m 300G -c 47 "/applications/R/R-4.0.0/bin/Rscript among_read_variation_scoring.R Col_0_deepsignalDNAmeth_30kb_90pc t2t-col.20210610 10000 500 CHG 0.50 0.20 Chr1"
+# csmit -m 200G -c 47 "/applications/R/R-4.0.0/bin/Rscript among_read_variation_scoring.R Col_0_deepsignalDNAmeth_30kb_90pc t2t-col.20210610 10000 500 CpG 0.50 0.20 Chr1"
 
 # Divide each read into adjacent segments each consisting of a given number of consecutive cytosines,
 # and calculate the methylation proportion for each segment of each read
@@ -20,7 +20,7 @@
 #genomeStepSize <- 500
 #context <- "CpG"
 #NAmax <- 0.50
-#CPUpc <- 0.66
+#CPUpc <- 0.20
 #chrName <- unlist(strsplit("Chr4", split = ","))
 
 args <- commandArgs(trailingOnly = T)
@@ -143,9 +143,9 @@ for(i in seq_along(chrs)) {
                         call = chr_tab[,9])
 
   chr_tab_GR_fwd <- chr_tab_GR[strand(chr_tab_GR) == "+"]
-  chr_tab_GR_fwd <- sort(chr_tab_GR_fwd, by = ~ read + start)
+#  chr_tab_GR_fwd <- sort(chr_tab_GR_fwd, by = ~ read + start)
   chr_tab_GR_rev <- chr_tab_GR[strand(chr_tab_GR) == "-"]
-  chr_tab_GR_rev <- sort(chr_tab_GR_rev, by = ~ read + start)
+#  chr_tab_GR_rev <- sort(chr_tab_GR_rev, by = ~ read + start)
 
   ## Note: findOverlaps() approach does not work where a window does not overlap
   ##       any positions in chr_tab_GR, which can occur with smaller genomeBinSize
